3840061e0f5537853200436954e1f2f084067cb5
belushkin <belushkin@belushkin-X550VB.(none)>
2014-10-20 16:46:46 +0300

    Added new r parsing script

---
+++ data/r/novus.r
@@ -0,0 +1,39 @@
+library(XML)
+library(scrapeR)
+library(stringr)
+
+getNumberOfPages <- function(urlParameter) {
+  pageSource <- scrape(url=urlParameter, headers=TRUE, parse=FALSE)
+  html <- htmlParse(pageSource, asText=TRUE, encoding='UTF-8')
+  pagination =  unlist(xpathSApply(html, '//div[@class="pagination pagination-centered"]', xmlValue))
+  number_of_pages_list = str_match_all(pagination, "\\d")
+  number_of_pages = length(unlist(number_of_pages_list))
+  return(number_of_pages)
+}
+
+getPageDataFrame <- function(urlParameter) {
+  pageSource <- scrape(url=urlParameter, headers=TRUE, parse=FALSE)
+  html <- htmlParse(pageSource, asText=TRUE, encoding='UTF-8')
+  
+  hrivna_price = unlist(xpathSApply(html, '//span[@class="grivna price"]', xmlValue))
+  kopiyki_price = unlist(xpathSApply(html, '//span[@class="kopeiki"]', xmlValue))
+  name = unlist(xpathSApply(html, '//div[@class="one-product-name"]', xmlValue))
+  
+  result = list()
+  for (i in 1:length(name)) {
+    vector = c(str_trim(name[i]), hrivna_price[i], kopiyki_price[i])
+    result = c(result, list(vector))
+  }
+  df <- data.frame(matrix(unlist(result), nrow=length(result), byrow=T))
+  return(df)
+}
+
+number_of_pages = getNumberOfPages('https://novus.zakaz.ua/ru/bakery/')
+for (i in 1:number_of_pages) {
+  df = getPageDataFrame(paste("https://novus.zakaz.ua/ru/bakery/?&page=",i,sep=""))
+  write.table(df, file = paste("/home/belushkin/sandbox/textmining/data/r/novus.", i, ".csv", sep=""), sep=',', col.names = FALSE)
+}
+
+
+
+
